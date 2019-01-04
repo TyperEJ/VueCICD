c33e2029b9f388bc65f91ed081c6aa468558dfa1
EJLin <ej@creatop.com.tw>
2019-01-04 17:31:57 +0800

    add trasform

--- src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>Hi,This is EJ</h1>
-  </div>
-</template>
---
+++ src/views/TransForm.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="trans-form">
+        <el-row type="flex" class="row-bg" justify="center">
+            <el-col :span="6">
+                <div class="grid-content bg-purple"></div>
+            </el-col>
+            <el-col :span="6">
+                <div class="grid-content bg-purple-light">
+                    <el-input
+                            type="textarea"
+                            :rows="10"
+                            placeholder="Pleas Input Something"
+                            v-model="currentInput"
+                    >
+                    </el-input>
+                </div>
+            </el-col>
+            <el-col :span="6">
+                <div class="grid-content bg-purple"></div>
+            </el-col>
+        </el-row>
+        <div style="margin: 20px 0;"></div>
+        <el-row type="flex" class="row-bg" justify="center">
+            <el-col :span="6">
+                <div class="grid-content bg-purple"></div>
+            </el-col>
+            <el-col :span="6">
+                <div class="grid-content bg-purple-light">
+                    <el-button @click="trans">Trans</el-button>
+                </div>
+            </el-col>
+            <el-col :span="6">
+                <div class="grid-content bg-purple"></div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'trans-form',
+        data() {
+            return {
+                currentInput: this.input
+            }
+        },
+        methods: {
+            trans() {
+                if(!this.currentInput.indexOf("[") &&
+                    !this.currentInput.indexOf("]")){
+                    return false
+                }
+
+                let list = this.currentInput.split('\n');
+                let result = '[\n';
+                list.forEach(function(item){
+                    result += '\''+item+'\',\n';
+                });
+                result += '];';
+                this.currentInput = result;
+            }
+        }
+    }
+</script>
